3f22d7939c15b7d2b75ff9393f39f7efce2fd03c
coxm <coxm@users.noreply.github.com>
2017-12-05 18:19:20 +0000

    Include phaser typings from tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-/// <reference path="../node_modules/phaser-ce/typescript/phaser.d.ts"/>
-
 import { Boot } from './states/boot';
 import { Loading } from './states/loading';
 import { Game } from './states/game';
